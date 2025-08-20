8950ba70743932a25fc894e7807ae291b17fb0ba
meng9273 <zhimengzheng1992@gmail.com>
2025-08-20 12:03:37 -0700

    start

---
+++ cleverpath-management/src/test/java/bcit/cst/CleverpathManagementApplicationTests.java
@@ -0,0 +1,15 @@
+package bcit.cst;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CleverpathManagementApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
